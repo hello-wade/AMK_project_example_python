58c259bbc9d4e896d1074d419eb855958a0a9fab
hello-wade <37059163+hello-wade@users.noreply.github.com>
2020-01-16 10:39:09 +0900

    add led_control.py
    파일 추가

---
+++ led_control.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(31, GPIO.OUT)
+
+while True:
+    GPIO.output(31, True)
+    print(“led on”)
+    time.sleep(2)
+
+    GPIO.output(31, False)
+    print(“led off”)
+    time.sleep(2)
